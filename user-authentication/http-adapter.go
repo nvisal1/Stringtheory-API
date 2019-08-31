01fc5e1ed44d8b991cee41a0254748b522389f44
Nick Visalli <nvisal1@students.towson.edu>
2019-08-31 10:57:22 -0400

    enable cors / preflight

--- user-authentication/http-adapter.go
+++ user-authentication/http-adapter.go
@@ -31,36 +31,40 @@ func (mha moduleHttpAdapter) InitializeAdapter() {
 // the variable of type UserToken is encoded and returned
 // to the client.
 func (mha moduleHttpAdapter) login(w http.ResponseWriter, req *http.Request) {
+	setupResponse(&w, req)
+	if req.Method == http.MethodOptions {
+		return
+	}
 	if req.Method == http.MethodPost {
 		b, err := ioutil.ReadAll(req.Body)
 		defer req.Body.Close()
 		if err != nil {
-			http.Error(w, http.StatusText(http.StatusInternalServerError) + " " + err.Error(), http.StatusInternalServerError)
+			http.Error(w, http.StatusText(http.StatusInternalServerError)+" "+err.Error(), http.StatusInternalServerError)
 			return
 		}
 
 		var u loginCredentials
 		err = json.Unmarshal(b, &u)
 		if err != nil {
-			http.Error(w, http.StatusText(http.StatusInternalServerError) + " " + err.Error(), http.StatusInternalServerError)
+			http.Error(w, http.StatusText(http.StatusInternalServerError)+" "+err.Error(), http.StatusInternalServerError)
 			return
 		}
 
 		ut, err := processLogin(u)
 		if err != nil {
-			http.Error(w, http.StatusText(http.StatusUnauthorized) + " " + err.Error(), http.StatusUnauthorized)
+			http.Error(w, http.StatusText(http.StatusUnauthorized)+" "+err.Error(), http.StatusUnauthorized)
 			return
 		}
 
 		e, err := json.Marshal(ut)
 		if err != nil {
-			http.Error(w, http.StatusText(http.StatusInternalServerError) + " " + err.Error(), http.StatusInternalServerError)
+			http.Error(w, http.StatusText(http.StatusInternalServerError)+" "+err.Error(), http.StatusInternalServerError)
 			return
 		}
 		w.Header().Set("content-type", "application/json")
 		w.Write(e)
 	} else {
-		http.Error(w, http.StatusText(http.StatusNotFound) + " Hint: try making a POST request to this endpoint", http.StatusNotFound)
+		http.Error(w, http.StatusText(http.StatusNotFound)+" Hint: try making a POST request to this endpoint", http.StatusNotFound)
 		return
 	}
 }
@@ -96,7 +100,13 @@ func (mha moduleHttpAdapter) register(w http.ResponseWriter, req *http.Request) 
 		w.Header().Set("content-type", "application/json")
 		w.Write(e)
 	} else {
-		http.Error(w, http.StatusText(http.StatusNotFound) + " Hint: try making a POST request to this endpoint", http.StatusNotFound)
+		http.Error(w, http.StatusText(http.StatusNotFound)+" Hint: try making a POST request to this endpoint", http.StatusNotFound)
 		return
 	}
+}
+
+func setupResponse(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
