7576639e70a4162d77de76b75a807ebdf5e04185
Nick Visalli <nvisal1@students.towson.edu>
2019-10-27 20:47:27 -0400

    add logic to complete a lesson

--- user-curriculum-progress/usecases/complete-exercise/complete-exercise.go
+++ user-curriculum-progress/usecases/complete-exercise/complete-exercise.go
@@ -9,18 +9,20 @@ import (
 	"time"
 )
 
-func CompleteExercise(requester shared.User, username string, exerciseID string, score float64) error {
+func CompleteExercise(requester *shared.User, username string, exerciseID string, score float64) error {
 	// check that requester is completed their own exercise
 	if requester.Username != username {
 		return errors.New("A user is not allowed to completed an exercise for another user")
 	}
-	// get exercise to verify existence
-	_, err := UserCurriculumProgressModule.ServiceCommunicator.GetExercise(exerciseID)
+
+	// get user to verify existence
+	_, err := UserCurriculumProgressModule.ServiceCommunicator.GetUser(username)
 	if err != nil {
 		return err
 	}
-	// get user to verify existence
-	_, err = UserCurriculumProgressModule.ServiceCommunicator.GetUser(username)
+
+	// get exercise to verify existence
+	exercise, err := UserCurriculumProgressModule.ServiceCommunicator.GetExercise(exerciseID)
 	if err != nil {
 		return err
 	}
@@ -33,18 +35,19 @@ func CompleteExercise(requester shared.User, username string, exerciseID string,
 	}
 
 	// send completed exercise message
-	message := createCompletedExerciseMessage(&requester, completedExercise)
+	message := createCompletedExerciseMessage(requester, completedExercise, exercise.LessonId)
 	UserCurriculumProgressModule.MessageStore.SendMessage(message)
 
 	return nil
 }
 
-func createCompletedExerciseMessage(requester *shared.User, completedExercise *CompletedExercise) *Message {
+func createCompletedExerciseMessage(requester *shared.User, completedExercise *CompletedExercise, lessonID string) *Message {
 	messageBody := fmt.Sprintf("User %s completed exercise %s", requester.Username, completedExercise.ExerciseId)
 	messageAttributes := make(map[string]interface{})
-	messageAttributes["title"] = "exercise-completed"
+	messageAttributes["title"] = "exercise completed"
 	messageAttributes["requester"] = requester
 	messageAttributes["exercise"] = completedExercise
+	messageAttributes["lessonID"] = lessonID
 	message := NewMessage(messageBody, messageAttributes)
 	return message
 }
--- user-curriculum-progress/usecases/complete-lesson/complete-lesson.go
+++ user-curriculum-progress/usecases/complete-lesson/complete-lesson.go
@@ -1,36 +1,54 @@
 package complete_lesson
 
 import (
-	user_curriculum_progress "Stringtheory-API/user-curriculum-progress"
+	"Stringtheory-API/shared"
+	. "Stringtheory-API/user-curriculum-progress/service-module"
+	. "Stringtheory-API/user-curriculum-progress/types"
+	"errors"
+	"fmt"
 	"time"
 )
 
-func CompleteLesson(uN string, lI string) error {
+func CompleteLesson(requester *shared.User, lessonID string) error {
+	// get requester to verify existence
+	_, err := UserCurriculumProgressModule.ServiceCommunicator.GetUser(requester.Username)
+	if err != nil {
+		return err
+	}
+
 	// get lesson to verify existence
-	l, err := user_curriculum_progress.getLesson(lI)
+	lesson, err := UserCurriculumProgressModule.ServiceCommunicator.GetLesson(lessonID)
 	if err != nil {
 		return err
 	}
+
 	// make sure that the user hasn't completed the lesson already
-
-	cL := user_curriculum_progress.completedLesson{
-		uN,
-		lI,
-		time.Now(),
+	_, err = UserCurriculumProgressModule.DataStore.GetCompletedLesson(requester.Username)
+	if err == nil {
+		return errors.New("user already completed the specified Lesson")
 	}
 
-	err = user_curriculum_progress.sm.DataStore().createCompletedLesson(cL)
+	completedLesson := NewCompletedLesson(requester.Username, lessonID, time.Now())
+
+	err = UserCurriculumProgressModule.DataStore.CreateCompletedLesson(completedLesson)
 	if err != nil {
 		return err
 	}
 
-	// check if all lessons were completed
-	// if yes - complete course
-	if !l.HasNext {
-		err = completeCourse(uN, l.CourseId)
-		if err != nil {
-			return err
-		}
-	}
+	// send completed lesson message
+	message := createCompletedLessonMessage(requester, completedLesson, lesson.CourseId)
+	UserCurriculumProgressModule.MessageStore.SendMessage(message)
+
 	return nil
+}
+
+func createCompletedLessonMessage(requester *shared.User, completedLesson *CompletedLesson, courseID string) *Message {
+	messageBody := fmt.Sprintf("User %s completed lesson %s", requester.Username, completedLesson.LessonId)
+	messageAttributes := make(map[string]interface{})
+	messageAttributes["title"] = "lesson completed"
+	messageAttributes["requester"] = requester
+	messageAttributes["lesson"] = completedLesson
+	messageAttributes["courseID"] = courseID
+	message := NewMessage(messageBody, messageAttributes)
+	return message
 }
